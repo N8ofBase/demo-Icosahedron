64372382a32dc5a478bf9407ca49bb0568e1b6c1
N8ofBase <n8ofbase@gmail.com>
2021-07-22 08:57:45 -0500

    Add files via upload

---
+++ demo-Icosahedron/src/main/java/com/prismcortex/demoIcosahedron/DemoIcosahedronApplication.java
@@ -0,0 +1,13 @@
+package com.prismcortex.demoIcosahedron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoIcosahedronApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoIcosahedronApplication.class, args);
+	}
+
+}
---
+++ demo-Icosahedron/src/test/java/com/prismcortex/demoIcosahedron/DemoIcosahedronApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prismcortex.demoIcosahedron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoIcosahedronApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
